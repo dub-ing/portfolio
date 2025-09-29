c78378f8aac6e0032d3ec4edd8fd50ef5af89089
dub-ing <duboyeolawale@gmail.com>
2025-09-29 10:15:48 -0700

    added skill component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,7 @@ import Image from "./Image";
 
 function Skills() {
   return (
-    <div class="bg-bat-signal rounded-xl py-3 px-3 flex flex-col justify-between md:col-start-2">
+    <div class="bg-bat-signal rounded-xl py-3 px-3 flex flex-col justify-between md:col-start-2 md:row-start-1">
       <div class="flex justify-between items-center
        md:w-[90%] md:m-auto">
         <span class="text-[#2e3a59] text-sm md:text-[1.2rem]">
